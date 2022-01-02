23d65ada0cfcad083c02c0af8b324c61d8bd147a
Grazielli Lima Berti <berti.grazielli@ibm.com>
2022-01-02 19:25:29 -0300

    fix images and font at ipad

--- AluraViagens/AluraViagens/Views/CellTravelView.swift
+++ AluraViagens/AluraViagens/Views/CellTravelView.swift
@@ -9,17 +9,24 @@ import SwiftUI
 
 struct CellTravelView: View {
     var viagem: Viagem
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
         VStack(alignment: .leading) {
             Text(viagem.titulo)
+                .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
             Image(viagem.imagem)
-                .resizable() //ocupa somente o espaço disponivel
-                .frame(height: 125)
+                .resizable()
+                .aspectRatio(contentMode: .fill) //zoom"
+                .frame(height: self.horizontalSizeClass == .compact ? 125 : 200)
+                .clipped() //corta a imagem baseada no tamanho disponível
+               
             HStack {
                 Text(viagem.quantidadeDeDias)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
                 Spacer()
                 Text(viagem.valor)
+                    .font(.custom("Avenir", size: self.horizontalSizeClass == .compact ? 14 : 24))
             }
         }
     }
