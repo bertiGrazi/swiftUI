7f0e2b7e0189b536bf2719412c4fe3e2d66a5743
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-27 15:41:14 -0300

    feat: build up the onboarding screen

--- Udemy/RestartApp/Restart/Restart/Screens/OnboardingView.swift
+++ Udemy/RestartApp/Restart/Restart/Screens/OnboardingView.swift
@@ -58,7 +58,54 @@ struct OnboardingView: View {
                 Spacer()
                 
                 //MARK: - Footer
-                
+                ZStack {
+                    //Part of the custom button
+                    
+                    //1. Background (static)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    //2. call-to-action (static)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    //3. capsule (Dynamic width)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    //4. Circle (Draggable)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding()
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardinViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //:HStack
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
             } //: VStack
         } //: ZStack
     }
