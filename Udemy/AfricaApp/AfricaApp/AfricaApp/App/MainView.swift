88308f578cf014675ed25c871a3ce8c6062a4a93
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-01 08:59:39 -0300

    Create app, create resources and launch screen

---
+++ Udemy/AfricaApp/AfricaApp/AfricaApp/App/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  AfricaApp
+//
+//  Created by Grazielli Berti on 01/02/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Udemy/AfricaApp/AfricaApp/AfricaApp/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  AfricaApp
+//
+//  Created by Grazielli Berti on 01/02/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
