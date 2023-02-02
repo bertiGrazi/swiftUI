0389469c586d3312dd93e6d36510777e20332149
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-02 08:15:04 -0300

    project setUp

---
+++ Kavsoft/TaskManager/TaskManagerApp/TaskManagerApp/TaskManagerApp/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TaskManagerApp
+//
+//  Created by Grazi  Berti on 02/02/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
