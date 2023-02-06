5d5f82a6ab1a3ca10d87c380d85b9a6b1767ba02
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-06 16:02:00 -0300

    start combine search

--- Kavsoft/Marvel/MarvelAPI/MarvelAPI/ViewModel/HomeViewModel.swift
+++ Kavsoft/Marvel/MarvelAPI/MarvelAPI/ViewModel/HomeViewModel.swift
@@ -6,7 +6,26 @@
 //
 
 import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var searchQuery = ""
+    
+    var searchCancellable: AnyCancellable? = nil
+    
+    init() {
+        searchCancellable = $searchQuery
+            .removeDuplicates()
+        //we dont need to fetch for every typing...
+        // so it will wait for 0.5 after user ends typing...
+            .debounce(for: 0.6, scheduler: RunLoop.main)
+            .sink(receiveValue: { str in
+                if str == "" {
+                    //reset Data...
+                } else {
+                    //search Data..
+                    print(str)
+                }
+            })
+    }
 }
