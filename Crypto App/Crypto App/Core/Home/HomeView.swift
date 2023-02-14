cabea032e6620bac0f09781fa1247e7651577e45
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-14 17:40:58 -0300

    App#002: Creating Homeview and a navigation header

--- Crypto App/Crypto App/Core/Home/HomeView.swift
+++ Crypto App/Crypto App/Core/Home/HomeView.swift
@@ -17,27 +17,9 @@ struct HomeView: View {
                 .ignoresSafeArea()
             
             VStack {
-                HStack {
-                    CircleButtonView(iconName: showPorfolio ? "plus" : "info")
-                        .animation(.none)
-                    Spacer()
-                    Text("Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                        .rotationEffect(Angle(degrees: showPorfolio ? 180 : 0))
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                showPorfolio.toggle()
-                            }
-                        }
-                }
-                .padding(.horizontal)
-                
+
+                homeHeader
                     Spacer(minLength: 0)
-                
             }
         }
     }
@@ -50,5 +32,28 @@ struct HomeView_Previews: PreviewProvider {
                 .navigationBarHidden(true)
         }
         .preferredColorScheme(.dark)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPorfolio ? "plus" : "info")
+                .animation(.none)
+            Spacer()
+            Text("Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPorfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPorfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
