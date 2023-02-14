0140d38a0526bd9d8e89994667daaf32c58f71b6
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-14 19:07:50 -0300

    SwiftfulThinking: How to use @Binding property wrapper in SwiftUI

--- Swiftful Thinking/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/Bootcamp(BegginerLevel)/BindingSwiftUIView.swift
+++ Swiftful Thinking/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/Bootcamp(BegginerLevel)/BindingSwiftUIView.swift
@@ -9,28 +9,47 @@ import SwiftUI
 
 struct BindingSwiftUIView: View {
     @State var backgroundColor: Color = Color.green
+    @State var title: String = "Title"
     
     var body: some View {
         ZStack {
             backgroundColor.edgesIgnoringSafeArea(.all)
             
-            Button(action: {
-                backgroundColor = Color.orange
-            }, label:  {
-                Text("Button")
+            
+            VStack {
+                Text(title)
                     .foregroundColor(.white)
-                    .padding()
-                    .padding(.horizontal)
-                    .background(.blue)
-                    .cornerRadius(10)
-            })
+                    .fontWeight(.bold)
+                    .padding(.vertical, 8)
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
+            }
         }
-                   
     }
 }
 
 struct BindingSwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         BindingSwiftUIView()
+    }
+}
+
+struct ButtonView: View {
+    @Binding var backgroundColor: Color
+    @Binding var title: String
+    @State var buttonColor: Color = Color.cyan
+    
+    var body: some View {
+        Button(action: {
+            backgroundColor = Color.orange
+            buttonColor = Color.pink
+            title = "NEW TITLE!!!!"
+        }, label:  {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(buttonColor)
+                .cornerRadius(10)
+        })
     }
 }
