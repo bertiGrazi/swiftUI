de0e8abe16f8662e44151071ab5ad8028bec7803
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-15 15:17:31 -0300

    App#004: Design a row to display coins in a List

--- Crypto App/Crypto App/Core/Home/CoinRowView.swift
+++ Crypto App/Crypto App/Core/Home/CoinRowView.swift
@@ -14,6 +14,38 @@ struct CoinRowView: View {
 
     var body: some View {
         HStack(spacing: 0) {
+            leftCoulm
+            Spacer()
+            if showHoldingColumn {
+                centerColum
+            }
+            rightColum
+        }
+        .font(.subheadline)
+        .padding()
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingColumn: true)
+                .preferredColorScheme(.light)
+                .previewInterfaceOrientation(.portrait)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin, showHoldingColumn: true)
+                .preferredColorScheme(.dark)
+                .previewInterfaceOrientation(.portrait)
+                .previewLayout(.sizeThatFits)
+        }
+      
+    }
+}
+
+extension CoinRowView {
+    private var leftCoulm: some View {
+        HStack(spacing: 0) {
             Text("\(coin.rank)")
                 .font(.caption)
                 .foregroundColor(Color.theme.secondaryText)
@@ -25,33 +57,28 @@ struct CoinRowView: View {
                 .font(.headline)
                 .padding(.leading, 6)
                 .foregroundColor(Color.theme.accent)
-            Spacer()
-            if showHoldingColumn {
-                VStack(alignment: .center) {
-                    Text(coin.currentHoldingsValue.asCurrencyWith6Decimals())
-                    Text((coin.currentHoldings ?? 0).asNumberString())
-                }
-                .foregroundColor(Color.theme.accent)
-            }
-            VStack(alignment: .trailing) {
-                Text(coin.currentPrice.asCurrencyWith6Decimals())
-                    .bold()
-                    .foregroundColor(Color.theme.accent)
-                Text(coin.priceChangePercentage24H?.asPorcentString() ?? "")
-                    .foregroundColor(
-                      (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                      Color.theme.green : Color.theme.red
-                    )
-            }
-            .frame(width: UIScreen.main.bounds.width / 3)
         }
     }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin, showHoldingColumn: true)
-            .preferredColorScheme(.dark)
-            .previewInterfaceOrientation(.portrait)
+    
+    private var centerColum: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColum: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPorcentString() ?? "")
+                .foregroundColor(
+                  (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                  Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing)
     }
 }
--- Crypto App/Crypto App/Extensions/Double.swift
+++ Crypto App/Crypto App/Extensions/Double.swift
@@ -9,6 +9,34 @@ import Foundation
 
 extension Double {
     
+    /// Convert a Double into a Currency with 2 decimal places
+    ///  ```
+    /// Convert 1234.46 to $1,234.56
+    ///  ```
+    
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current // <- default value
+        formatter.currencyCode = "usd" // <- chenge current
+        formatter.currencySymbol = "$" // <- chenge current symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    
+    /// Convert a Double into a Currency  as String with 2 decimal places
+    ///  ```
+    /// Convert 1234.46 to "$1,234.56"
+    ///  ```
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     /// Convert a Double into a Currency with 2-6 decimal places
     ///  ```
     /// Convert 1234.46 to $1,234.56
