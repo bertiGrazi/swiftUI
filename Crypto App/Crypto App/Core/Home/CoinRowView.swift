4fe2766770c21aafb860efaa467bbca2614e5dee
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-15 14:31:43 -0300

    call mock in our CoinRowView

--- Crypto App/Crypto App/Core/Home/CoinRowView.swift
+++ Crypto App/Crypto App/Core/Home/CoinRowView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct CoinRowView: View {
     
-    
+    let coin: CoinModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(coin.name)
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView()
+        CoinRowView(coin: dev.coin)
     }
 }
