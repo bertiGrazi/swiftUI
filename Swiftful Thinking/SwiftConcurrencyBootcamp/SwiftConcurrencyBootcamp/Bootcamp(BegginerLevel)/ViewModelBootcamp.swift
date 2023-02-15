3fa3080addd3bd757e0d78378650418255a01569
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-15 08:21:03 -0300

    create simple list fruint

---
+++ Swiftful Thinking/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/Bootcamp(BegginerLevel)/ViewModelBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  ViewModelBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Grazi  Berti on 15/02/23.
+//
+
+import SwiftUI
+
+struct FruitModel: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
+}
+
+struct ViewModelBootcamp: View {
+    @State var fruitArray: [FruitModel] = []
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruitArray) { fruit in
+                    HStack {
+                        Text("\(fruit.count)")
+                            .foregroundColor(.red)
+                        Text(fruit.name)
+                            .font(.headline)
+                            .bold()
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Fruint List")
+            .onAppear {
+                getFruit()
+            }
+        }
+    }
+    
+    func getFruit() {
+        let fruit1 = FruitModel(name: "Orange", count: 1)
+        let fruit2 = FruitModel(name: "Banana", count: 2)
+        let fruit3 = FruitModel(name: "Watermelon", count: 88)
+        
+        fruitArray.append(fruit1)
+        fruitArray.append(fruit2)
+        fruitArray.append(fruit3)
+    }
+}
+
+struct ViewModelBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModelBootcamp()
+    }
+}
