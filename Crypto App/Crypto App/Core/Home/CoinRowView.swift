40f2ac2cbdc8950de88c7354465b8d7b21b7aba7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-15 14:29:55 -0300

    create mock for Coin Row

---
+++ Crypto App/Crypto App/Core/Home/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  Crypto App
+//
+//  Created by Grazi  Berti on 15/02/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
