64b3b4d62c18c04749ac401d59e0ad7b947d5223
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-20 15:41:34 -0300

    create enum for networking error

--- Crypto App/Crypto App/Utilities/NetworkingManager.swift
+++ Crypto App/Crypto App/Utilities/NetworkingManager.swift
@@ -9,19 +9,34 @@ import Foundation
 import Combine
 
 class NetworkingManager {
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknoun
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🍎] Bad response from URL \(url)"
+            case .unknoun: return "[⚠️] Unknown error occured"
+                
+            }
+        }
+    }
+    
     /// adicionando o static nunca iremos precisar primeiro instanciar NetworkingManager e depois acessar a função download. Dessa maneira, fazendo o acesso da função diretamente
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ try handleURLResponse(output: $0) })
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
         //converte em qualquer retorno de Publishers
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        //// just see
+       // throw NetworkingError.badURLResponse(url: url)
         guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badURLResponse(url: url)
         }
         return output.data
     }
