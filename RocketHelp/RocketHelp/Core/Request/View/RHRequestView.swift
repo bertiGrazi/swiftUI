a5e5ef4f3c721709128c3eade3e7dc93bf80ac75
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-21 15:25:39 -0300

    organizing request view

--- RocketHelp/RocketHelp/Core/Request/View/RHRequestView.swift
+++ RocketHelp/RocketHelp/Core/Request/View/RHRequestView.swift
@@ -20,68 +20,18 @@ struct RHRequestView: View {
                     .ignoresSafeArea()
                 
             VStack(spacing: 0) {
-                // Header
-                    HStack {
-                        Image("logo_header")
-                        
-                        Spacer(minLength: 0)
-                        Button {
-                            print("login out")
-                        } label: {
-                            Image(systemName: "rectangle.portrait.and.arrow.right")
-                                .foregroundColor(Color("Background_LightGray"))
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    .frame(height: 80)
-                    .background(Color("Background_Gray"))
+                    headerView
                     
                     Spacer(minLength: 0)
            
                 VStack(spacing: 10) {
                     // Label and count Request
-                    HStack {
-                        Text("Solicitações")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                            .font(.title3)
-                        
-                        Spacer(minLength: 0)
-                        
-                        Text("0")
-                            .foregroundColor(Color("Background_LightGray"))
-                            .font(.title3)
-                    }
+                    leabelRequest
                     .padding(.horizontal)
                     
                     //Segment Bar
-                    HStack(spacing: 5) {
-                        ForEach(tabs, id: \.self) { tab in
-                            Text(tab)
-                                .font(.callout)
-                                .foregroundColor(requestViewModel.currentTab == tab ? Color("Yellow") : Color("Background_LightGray"))
-                                .padding(.vertical, 10)
-                                .frame(maxWidth: .infinity)
-                                .background {
-                                    Color("Background_Gray")
-                                    if requestViewModel.currentTab == tab {
-                                        Rectangle()
-                                            .stroke(
-                                                requestViewModel.currentTab == tab ? Color("Yellow") : Color("Background_Gray")
-                                            )
-                                            .matchedGeometryEffect(id: "TAB", in: animation)
-                                    }
-                                }
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    requestViewModel.currentTab = tab
-                                }
-                        }
-                        .padding(.horizontal)
-                    }
+                    requestSegmentBar
                     .padding(.vertical, 10)
-                    
-                    //Spacer()
                     
                     //Card List
                     VStack(spacing: 16) {
@@ -100,7 +50,7 @@ struct RHRequestView: View {
             
             }
                 //Buttom
-                RHButtonView(textButton: "NOVA SOLICITAÇÃO", actionButton: {})
+                RHButtonView(textButton: "Nova solicitação", actionButton: {})
             }
             .navigationBarBackButtonHidden(true)
     }
@@ -112,6 +62,67 @@ struct RHRequestView_Previews: PreviewProvider {
             RHRequestView()
                 .navigationBarHidden(true)
                 .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+extension RHRequestView {
+    private var headerView: some View {
+        HStack {
+            Image("logo_header")
+            
+            Spacer(minLength: 0)
+            Button {
+                print("login out")
+            } label: {
+                Image(systemName: "rectangle.portrait.and.arrow.right")
+                    .foregroundColor(Color("Background_LightGray"))
+            }
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 80)
+        .background(Color("Background_Gray"))
+    }
+    
+    private var leabelRequest: some View {
+        HStack {
+            Text("Solicitações")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .font(.title3)
+            
+            Spacer(minLength: 0)
+            
+            Text("0")
+                .foregroundColor(Color("Background_LightGray"))
+                .font(.title3)
+        }
+    }
+    
+    private var requestSegmentBar: some View {
+        HStack(spacing: 5) {
+            ForEach(tabs, id: \.self) { tab in
+                Text(tab)
+                    .font(.callout)
+                    .foregroundColor(requestViewModel.currentTab == tab ? Color("Yellow") : Color("Background_LightGray"))
+                    .padding(.vertical, 10)
+                    .frame(maxWidth: .infinity)
+                    .background {
+                        Color("Background_Gray")
+                        if requestViewModel.currentTab == tab {
+                            Rectangle()
+                                .stroke(
+                                    requestViewModel.currentTab == tab ? Color("Yellow") : Color("Background_Gray")
+                                )
+                                .matchedGeometryEffect(id: "TAB", in: animation)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        requestViewModel.currentTab = tab
+                    }
+            }
+            .padding(.horizontal)
         }
     }
 }
