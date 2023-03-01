868f955f187782a857f5917a03ef3ea7da3624dd
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-02-28 21:24:04 -0300

    textfield in new request view

--- RocketHelp/RocketHelp/Core/NewRequest/View/NewRequestView.swift
+++ RocketHelp/RocketHelp/Core/NewRequest/View/NewRequestView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct NewRequestView: View {
     @State var assetNumber: String = ""
+    @State var problemDescription: String = ""
     
     var body: some View {
         ZStack(alignment: .bottom) {
             Color("Background_Gray")
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 10) {
                 headerView
                 
-                VStack {
-                    RHTextFieldView(
-                        textField: TextField("Número do Patrimônio", text: $assetNumber),
-                        imageName: "",
-                        height: 56,
-                        typeKeyborad: .decimalPad
-                    )
-                    .padding()
+                VStack(spacing: 32) {
+                    TextField("Número do Patrimônio", text: self.$assetNumber)
+                        .frame(height: 56)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .padding([.horizontal], 16)
+                        .cornerRadius(6)
+                        .background(Color("Background"))
+                        .padding([.horizontal], 24)
+                    
+                    
+                    TextField("Descrição do problema", text: self.$assetNumber)
+                        .frame(height: 400)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .padding([.horizontal], 16)
+                        .cornerRadius(6)
+                        .background(Color("Background"))
+                        .padding([.horizontal], 24)
                 }
+                
+                Spacer()
+                
+                //Buttom
+                RHButtonView(textButton: "Nova solicitação", actionButton: {})
             }
+            
         }
     }
 }
@@ -49,8 +65,6 @@ extension NewRequestView {
                     .padding()
             }
             
-            
-            
             Text("Solicitação")
                 .foregroundColor(.white)
                 .font(.title3)
@@ -58,7 +72,7 @@ extension NewRequestView {
             
             Spacer(minLength: 0)
         }
-        .padding(.horizontal, 20)
+        .padding(.horizontal, 10)
         .frame(height: 88)
         .background(Color("Background_Gray"))
     }
