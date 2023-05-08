e4adf71c667ba0c26c6b887e4988ab574c5d72f7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-05-08 09:40:58 -0300

    install firebase

---
+++ iOS Academy/SwiftUI To Do List/ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Grazi  Berti on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
