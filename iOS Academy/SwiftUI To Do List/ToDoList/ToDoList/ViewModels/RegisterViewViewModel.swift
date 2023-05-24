87dd2fb2a93eded75740ee3a74fe0b63d5fc967e
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-05-24 17:49:57 -0300

    validate user register

--- iOS Academy/SwiftUI To Do List/ToDoList/ToDoList/ViewModels/RegisterViewViewModel.swift
+++ iOS Academy/SwiftUI To Do List/ToDoList/ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -14,16 +14,27 @@ class RegisterViewViewModel: ObservableObject {
     
     init() {}
     
-    func login() {
-        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
-              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+    func register() {
+        guard validate() else {
             return
         }
-        
-        print("Called")
     }
     
-    func validate() {
+    func validate() -> Bool {
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
         
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
     }
 }
