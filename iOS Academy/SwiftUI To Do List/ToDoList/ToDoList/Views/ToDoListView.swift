a86f13d05798c03d86a0bc0f86b9606708422bcd
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-05-25 09:05:39 -0300

    create account view ui

--- iOS Academy/SwiftUI To Do List/ToDoList/ToDoList/Views/ToDoListView.swift
+++ iOS Academy/SwiftUI To Do List/ToDoList/ToDoList/Views/ToDoListView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewModel()
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     var body: some View {
-        Text("Welcome to your acount!")
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    // Action
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
+        }
     }
 }
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView()
+        ToDoListView(userId: ")
     }
 }
